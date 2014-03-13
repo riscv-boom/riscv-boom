c7cbdcf12d6525c0bdedfd636d9f7c602203b111
Christopher Celio <celio@eecs.berkeley.edu>
2014-03-12 19:40:22 -0700

    Clarified consts regarding ROB.

--- src/main/scala/consts.scala
+++ src/main/scala/consts.scala
@@ -66,7 +66,8 @@ trait BOOMProcConstants
    val DC_NUM_MSHR = 2    // secondary miss handler
 
    val INTEGER_ISSUE_SLOT_COUNT = 12
-   val NUM_ROB_ENTRIES          = 14*DECODE_WIDTH // number of ROB entries (32 entries for R10k)
+   val NUM_ROB_ROWS             = 14; require (NUM_ROB_ROWS % 2 == 0);
+   val NUM_ROB_ENTRIES          = NUM_ROB_ROWS*DECODE_WIDTH // number of ROB entries (32 entries for R10k)
    val NUM_LSU_ENTRIES          = 8   // number of LD/ST entries
    require (isPow2(NUM_LSU_ENTRIES))
    val ROB_ADDR_SZ = log2Up(NUM_ROB_ENTRIES) 
