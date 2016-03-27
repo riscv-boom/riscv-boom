223d0eaedaf8530afafebfc7dc239704425b61dc
Christopher Celio <celio@eecs.berkeley.edu>
2016-03-27 02:06:07 -0700

    moved core.scala code into microop.scala

--- src/main/scala/core.scala
+++ src/main/scala/core.scala
@@ -32,12 +32,8 @@ import Node._
 import cde.Parameters
 
 import rocket.Instructions._
-import rocket.ALU._
 import rocket.Str
 import uncore.HtifIO
-import FUCode._
-
-import scala.collection.mutable.ArrayBuffer
 
 
 abstract class BoomModule(implicit val p: Parameters) extends Module
@@ -46,121 +42,6 @@ abstract class BoomModule(implicit val p: Parameters) extends Module
 class BoomBundle(implicit val p: Parameters) extends junctions.ParameterizedBundle()(p)
   with HasBoomCoreParameters
 
-
-//-------------------------------------------------------------
-// NOTE: I can't promise these signals get killed/cleared on a mispredict,
-// so I should listen to the corresponding valid bit
-// For example, on a bypassing, we listen to rf_wen to see if bypass is valid,
-// but we "could" be bypassing to a branch which kills us (a false positive combinational loop),
-// so we have to keep the rf_wen enabled, and not dependent on a branch kill signal
-// TODO REFACTOR this, as this should no longer be true, as bypass occurs in stage before branch resolution
-class CtrlSignals extends Bundle()
-{
-   val br_type     = UInt(width = BR_N.getWidth)
-   val op1_sel     = UInt(width = OP1_X.getWidth)
-   val op2_sel     = UInt(width = OP2_X.getWidth)
-   val imm_sel     = UInt(width = IS_X.getWidth)
-   val op_fcn      = Bits(width = SZ_ALU_FN)
-   val fcn_dw      = Bool()
-   val rf_wen      = Bool()
-   val csr_cmd     = Bits(width = rocket.CSR.SZ)
-   val is_load     = Bool()   // will invoke TLB address lookup
-   val is_sta      = Bool()   // will invoke TLB address lookup
-   val is_std      = Bool()
-}
-
-class DebugStageEvents extends Bundle()
-{
-   // Track the sequence number of each instruction fetched.
-   val fetch_seq        = UInt(width = 32)
-}
-
-class MicroOp(implicit p: Parameters) extends BoomBundle()(p)
-{
-   val valid            = Bool()                      // is this uop valid? or has it been masked out,
-                                                      // used by fetch buffer and Decode stage
-   val iw_state         = UInt(width = 2)             // what is the next state of this uop in the issue window? useful
-                                                      // for the compacting queue? TODO or is this not really belong
-                                                      // here?
-
-   val uopc             = Bits(width = UOPC_SZ)       // micro-op code
-   val inst             = Bits(width = 32)
-   val pc               = UInt(width = coreMaxAddrBits)
-   val fu_code          = Bits(width = FUC_SZ)        // which functional unit do we use?
-   val ctrl             = new CtrlSignals
-
-   val wakeup_delay     = UInt(width = log2Up(MAX_WAKEUP_DELAY)) // unused
-   val allocate_brtag   = Bool()                      // does this allocate a branch tag? (is branch or JR but not JAL)
-   val is_br_or_jmp     = Bool()                      // is this micro-op a (branch or jump) vs a regular PC+4 inst?
-   val is_jump          = Bool()                      // is this a jump? (jal or jalr)
-   val is_jal           = Bool()                      // is this a JAL (doesn't include JR)? used for branch unit
-   val is_ret           = Bool()                      // is jalr with rd=x0, rs1=x1? (i.e., a return)
-   val is_call          = Bool()                      //
-   val br_mask          = Bits(width = MAX_BR_COUNT)  // which branches are we being speculated under?
-   val br_tag           = UInt(width = BR_TAG_SZ)
-
-   val br_was_mispredicted = Bool()                   // (for stat tracking)
-   val br_prediction    = new BranchPrediction
-
-   val fetch_pc_lob     = UInt(width = log2Up(FETCH_WIDTH*coreInstBytes)) // track which PC was used to fetch this
-                                                                          // instruction
-
-
-   val imm_packed       = Bits(width = LONGEST_IMM_SZ) // densely pack the imm in decode...
-                                                       // then translate and sign-extend in execute
-   val csr_addr         = UInt(width = CSR_ADDR_SZ)    // only used for critical path reasons in Exe
-   val rob_idx          = UInt(width = ROB_ADDR_SZ)
-   val ldq_idx          = UInt(width = MEM_ADDR_SZ)
-   val stq_idx          = UInt(width = MEM_ADDR_SZ)
-   val brob_idx         = UInt(width = BROB_ADDR_SZ)
-   val pdst             = UInt(width = PREG_SZ)
-   val pop1             = UInt(width = PREG_SZ)
-   val pop2             = UInt(width = PREG_SZ)
-   val pop3             = UInt(width = PREG_SZ)
-
-   val prs1_busy        = Bool()
-   val prs2_busy        = Bool()
-   val prs3_busy        = Bool()
-   val stale_pdst       = UInt(width = PREG_SZ)
-   val exception        = Bool()
-   val exc_cause        = UInt(width = xLen)          // TODO compress this down, xlen is insanity
-   val bypassable       = Bool()                      // can we bypass ALU results? (doesn't include loads, csr, etc...)
-   val mem_cmd          = UInt(width = 4)             // sync primitives/cache flushes
-   val mem_typ          = UInt(width = 3)             // memory mask type for loads/stores
-   val is_fence         = Bool()
-   val is_fencei        = Bool()
-   val is_store         = Bool()                      // anything that goes into the STQ, including fences and AMOs
-   val is_amo           = Bool()
-   val is_load          = Bool()
-   val is_unique        = Bool()                      // only allow this instruction in the pipeline, wait for STQ to
-                                                      // drain, clear fetcha fter it (tell ROB to un-ready until empty)
-   val flush_on_commit  = Bool()                      // some instructions need to flush the pipeline behind them
-
-   // logical specifiers (only used in Decode->Rename), except rollback (ldst)
-   val ldst             = UInt(width=LREG_SZ)
-   val lrs1             = UInt(width=LREG_SZ)
-   val lrs2             = UInt(width=LREG_SZ)
-   val lrs3             = UInt(width=LREG_SZ)
-   val ldst_val         = Bool()              // is there a destination? invalid for stores, rd==x0, etc. TODO is there anytime the destination is pass through?
-   val dst_rtype        = UInt(width=2)
-   val lrs1_rtype       = UInt(width=2)
-   val lrs2_rtype       = UInt(width=2)
-   val frs3_en          = Bool()
-
-   // floating point information
-   val fp_val           = Bool()             // is a floating-point instruction (F- or D-extension)? If it's non-ld/st it will write back exception bits to the fcsr
-   val fp_single        = Bool()             // single-precision floating point instruction (F-extension)
-
-   // exception information
-   val xcpt_if          = Bool()
-
-   // purely debug information
-   val debug_wdata      = Bits(width=xLen)
-   val debug_ei_enabled = Bool()
-   val debug_events     = new DebugStageEvents
-
-   def fu_code_is(_fu: Bits) = fu_code === _fu
-}
 
 class FetchBundle(implicit p: Parameters) extends BoomBundle()(p)
 {
@@ -231,7 +112,7 @@ class BOOMCore(implicit p: Parameters) extends BoomModule()(p)
    // Instruction Decode State
    val dec_valids     = Wire(Vec(DECODE_WIDTH, Bool()))  // are the decoded instruction valid? It may be held up though.
    val dec_uops       = Wire(Vec(DECODE_WIDTH, new MicroOp()))
-   val dec_will_fire  = Wire(Vec(DECODE_WIDTH, Bool()))  // can the instruction fire beyond decode? 
+   val dec_will_fire  = Wire(Vec(DECODE_WIDTH, Bool()))  // can the instruction fire beyond decode?
                                                          // (can still be stopped in ren or dis)
    val dec_rdy        = Wire(Bool())
    val rob_rdy        = Wire(Bool())
@@ -270,7 +151,7 @@ class BOOMCore(implicit p: Parameters) extends BoomModule()(p)
 
    // Writeback State
 
-   
+
    // Commit Stage
    val com_valids            = Wire(Vec(DECODE_WIDTH, Bool()))
    val com_uops              = Wire(Vec(DECODE_WIDTH, new MicroOp()))
@@ -287,8 +168,8 @@ class BOOMCore(implicit p: Parameters) extends BoomModule()(p)
    val csr_interrupt         = Wire(Bool())
    val csr_interrupt_cause   = Wire(UInt())
    val watchdog_trigger      = Wire(Bool())
- 
-   
+
+
    //****************************************
    // Time Stamp Counter & Retired Instruction Counter
    // (only used for printf and vcd dumps - the actual counters are in the CSRFile)
@@ -299,7 +180,7 @@ class BOOMCore(implicit p: Parameters) extends BoomModule()(p)
    irt_reg  := irt_reg + PopCount(com_valids.toBits)
    debug(tsc_reg)
    debug(irt_reg)
-    
+
 
    //****************************************
    // Print-out information about the machine
@@ -1183,7 +1064,7 @@ class BOOMCore(implicit p: Parameters) extends BoomModule()(p)
 //      csr.io.uarch_counters(10) := lsu_io.counters.ld_killed
       csr.io.uarch_counters(10) := lsu_io.counters.ld_order_fail
 
-//      csr.io.uarch_counters(11) := 
+//      csr.io.uarch_counters(11) :=
 
 
       csr.io.uarch_counters(11) := br_unit.bpd_update.valid // provide base-line on the number of updates, vs mispredicts
