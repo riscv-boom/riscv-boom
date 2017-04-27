0b6bf25ddb3bc8048e23272bff860f5365e7d31c
Christopher Celio <celio@eecs.berkeley.edu>
2017-04-26 20:07:19 -0700

    [util] Add a flow Queue for Microops.
       * The trick is we must keep updating the branch masks.
       * Supress deq.valid if a branch or flush killed the entry a we drain the
          queue.

--- src/main/scala/consts.scala
+++ src/main/scala/consts.scala
@@ -30,7 +30,7 @@ trait BOOMDebugConstants
    val DEBUG_PRINTF_LSU    = true && DEBUG_PRINTF
    val DEBUG_PRINTF_ROB    = true && DEBUG_PRINTF
    val DEBUG_PRINTF_TAGE   = true && DEBUG_PRINTF
-   val DEBUG_PRINTF_BROB   = true && DEBUG_PRINTF
+   val DEBUG_PRINTF_BROB   = false && DEBUG_PRINTF
 
    // BPD asserts can be too heavy-handed for something that we're okay giving incorrect answers.
    val ENABLE_BPD_ASSERTS  = false
