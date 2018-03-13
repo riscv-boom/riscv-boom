37146cabb24559fe7d6148008701f381029491d2
Christopher Celio <christopher.celio@gmail.com>
2018-03-13 14:33:07 -0700

    [imul] Convert to chisel3.

--- src/main/scala/exu/imul.scala
+++ src/main/scala/exu/imul.scala
@@ -4,7 +4,8 @@
 //------------------------------------------------------------------------------
 
 package boom
-import Chisel._
+import chisel3._
+import chisel3.util._
 
 import freechips.rocketchip.rocket.ALU._
 // TODO can repurpose the FN_* ctrl signals,
@@ -13,12 +14,12 @@ import freechips.rocketchip.rocket.ALU._
 class IMul(imul_stages: Int) extends Module
 {
    val io = IO(new Bundle {
-      val valid = Bool(INPUT)
-      val fn  = UInt(INPUT, SZ_ALU_FN)
-      val dw  = UInt(INPUT, SZ_DW)
-      val in0 = UInt(INPUT,  64)
-      val in1 = UInt(INPUT,  64)
-      val out = UInt(OUTPUT, 64)
+      val valid = Input(Bool())
+      val fn  = Input  (UInt(width=SZ_ALU_FN.W))
+      val dw  = Input  (UInt(width=SZ_DW.W))
+      val in0 = Input  (UInt(width=64.W))
+      val in1 = Input  (UInt(width=64.W))
+      val out = Output (UInt(width=64.W))
    })
 
    def FN(dw: UInt, fn: UInt) = io.dw === dw && io.fn === fn
@@ -32,16 +33,16 @@ class IMul(imul_stages: Int) extends Module
    val lhs = Cat(
       io.in0(63) & sxl64,
       Fill(32, ~zxl32)&io.in0(63,32) | Fill(32, sxl32&io.in0(31)),
-      io.in0(31,0)) //TODO: 65 bits
+      io.in0(31,0)) // 65 bits
    val rhs = Cat(
       io.in1(63) & sxr64,
       Fill(32, ~zxr32)&io.in1(63,32) | Fill(32, sxr32&io.in1(31)),
-      io.in1(31,0)) //TODO: 65 bits
+      io.in1(31,0)) // 65 bits
 
-   val mul_result = lhs.asSInt * rhs.asSInt //TODO:130 bits
+   val mul_result = lhs.asSInt * rhs.asSInt // 130 bits
 
    val mul_output_mux = MuxCase(
-      UInt(0, 64), Array(
+      0.asUInt(64.W), Array(
          FN(DW_64, FN_MUL)    -> mul_result(63,0),
          FN(DW_64, FN_MULH)   -> mul_result(127,64),
          FN(DW_64, FN_MULHU)  -> mul_result(127,64),
