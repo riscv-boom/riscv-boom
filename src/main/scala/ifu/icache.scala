8c843c19d32e00ae50427f30d315707a4a9223aa
Christopher Celio <christopher.celio@gmail.com>
2018-04-21 12:10:05 -0700

    [icache] Add ability to BlackBox the ICache.
    Currently, to emit the BlackBox version of ICache, one must change the default
    input parameter enableBlackBox in the ICache constructor.
    
    BlackBoxing the ICache is somewhat challenging as the ICache is a LazyModule and
    needs to do its job of negotiating and wiring stuff up. Likewise, it's also not
    (currently) possible to BlackBox the LazyModuleImp, so the current solution is
    to place the BlackBox inside of the LazyModuleImp, adding an extra level to the
    hierarchy.
    
    There is an additional challenge as the straight-forward solution leads to
    firrtl-emitter crashes on a None.get. The current work-around is to add an extra
    level of indirection around the IO bundle.

--- src/main/scala/ifu/icache.scala
+++ src/main/scala/ifu/icache.scala
@@ -24,9 +24,7 @@ import boom.common._
 //    rowBits: Int = 128,
 //    nTLBEntries: Int = 32,
 //    cacheIdBits: Int = 0,
-//    tagECC: Option[String] = None,
-//    dataECC: Option[String] = None,
-//    itimAddr: Option[BigInt] = None,
+//    tagECC: Option[String] = None, //    dataECC: Option[String] = None, //    itimAddr: Option[BigInt] = None,
 //    prefetch: Boolean = false,
 //    blockBytes: Int = 64,
 //    latency: Int = 2,
@@ -36,8 +34,16 @@ import boom.common._
 //  def replacement = new RandomReplacement(nWays)
 //}
 
-class ICache(val icacheParams: ICacheParams, val hartId: Int)(implicit p: Parameters) extends LazyModule {
-  lazy val module = new ICacheModule(this)
+class ICache(
+    val icacheParams: ICacheParams,
+    val hartId: Int,
+    val enableBlackBox: Boolean = true)(implicit p: Parameters)
+  extends LazyModule
+{
+  lazy val module: ICacheBaseModule = if (!enableBlackBox)
+    new ICacheModule(this)
+  else
+    new ICacheModuleBlackBox(this)
   val masterNode = TLClientNode(Seq(TLClientPortParameters(Seq(TLClientParameters(
     sourceId = IdRange(0, 1 + icacheParams.prefetch.toInt), // 0=refill, 1=hint
     name = s"Core ${hartId} ICache")))))
@@ -96,13 +102,16 @@ object GetPropertyByHartId {
   }
 }
 
-@chiselName
-class ICacheModule(outer: ICache) extends LazyModuleImp(outer)
+abstract class ICacheBaseModule(outer: ICache) extends LazyModuleImp(outer)
   with HasL1ICacheBankedParameters
 {
   override val cacheParams = outer.icacheParams // Use the local parameters
-
   val io = IO(new ICacheBundle(outer))
+}
+
+@chiselName
+class ICacheModule(outer: ICache) extends ICacheBaseModule(outer)
+{
   val (tl_out, edge_out) = outer.masterNode.out(0)
   // Option.unzip does not exist :-(
   val (tl_in, edge_in) = outer.slaveNode.in.headOption.unzip
@@ -578,3 +587,26 @@ class ICacheModule(outer: ICache) extends LazyModuleImp(outer)
     "\n   " + (if (nBanks == 2) "Dual-banked" else "Single-banked") +
     "\n   I-TLB entries : " + cacheParams.nTLBEntries + "\n"
 }
+
+
+// Provide a BlackBox version of the ICache.
+// NOTE: we can't really BlackBox a LazyModule, so instead we use the
+// LazyModuleImp as a thin shim around the actual BlackBox itself.
+// However, we have to provide another level of indirection through the IOs to
+// avoid an emitter error (which may be related to Option()s).
+class ICacheModuleBlackBox(outer: ICache) extends ICacheBaseModule(outer)
+{
+  val icachebb = Module(new ICacheBlackBox(outer))
+  io <> icachebb.io.signals
+}
+
+class ICacheBlackBox(outer: ICache) extends BlackBox
+{
+  val io = IO(new ICacheBundleShim(outer))
+}
+
+class ICacheBundleShim(val outer: ICache) extends CoreBundle()(outer.p)
+{
+  val signals = new ICacheBundle(outer)
+}
+
