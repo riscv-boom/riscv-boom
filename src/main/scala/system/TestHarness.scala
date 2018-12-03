e842ba9e8e40b9946a37ce61c69eab0d394483a7
abejgonzalez <abe.j.gonza@gmail.com>
2018-12-03 13:51:44 -0800

    Updated TestHarness.scala to Chisel3

--- src/main/scala/system/TestHarness.scala
+++ src/main/scala/system/TestHarness.scala
@@ -4,7 +4,7 @@ package boom.system
 
 //TODO: Figure out how to switch to Chisel3
 
-import Chisel._
+import chisel3._
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy.LazyModule
 import freechips.rocketchip.devices.debug.Debug
@@ -12,18 +12,19 @@ import freechips.rocketchip.devices.debug.Debug
 
 class TestHarness()(implicit p: Parameters) extends Module {
   val io = IO(new Bundle {
-    val success = Bool(OUTPUT)
+    val success = Output(Bool())
   })
   println("\n\nBuilding TestHarness for an ExampleBoomSystem.\n")
 
   val dut = Module(LazyModule(new ExampleBoomSystem).module)
-  dut.reset := reset | dut.debug.ndreset
+  dut.reset := reset.toBool | dut.debug.ndreset
 
   dut.dontTouchPorts()
   dut.tieOffInterrupts()
   dut.connectSimAXIMem()
   dut.connectSimAXIMMIO()
+  dut.l2_frontend_bus_axi4.foreach( q => q := DontCare ) // Overridden in next line
   dut.l2_frontend_bus_axi4.foreach(_.tieoff)
 
-  Debug.connectDebug(dut.debug, clock, reset, io.success)
+  Debug.connectDebug(dut.debug, clock, reset.toBool, io.success)
 }
