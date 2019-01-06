126a19e0cc7790bb267b51835a0f8261e4f7edc9
Jerry Zhao <jerryz123@berkeley.edu>
2019-01-05 21:00:47 -0800

    [ci] Pull prebuilt release of riscv-tools

--- ci/prepare-riscv-tools.sh
+++ ci/prepare-riscv-tools.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# create the riscv tools binaries from riscv-boom/boom-template
+# Get riscv-tools prebuilt from firesim
 
 # turn echo on and error on earliest command
 set -x
 set -e
 
-# move to top level dir and remove boom-template if present
-cd ..
-rm -rf boom-template
-
-# clone boom-template and create the riscv-tools
-git clone --progress --verbose https://github.com/riscv-boom/boom-template.git
-cd boom-template
-git submodule update --init rocket-chip
-(cd rocket-chip && git submodule update --init riscv-tools)
-(cd rocket-chip/riscv-tools && git submodule update --init --recursive riscv-isa-sim riscv-fesvr riscv-pk riscv-opcodes riscv-tests riscv-gnu-toolchain riscv-openocd)
-
-# build the tools
-cd rocket-chip/riscv-tools
-./build.sh
-
+# clone and install
+git clone --progress --verbose https://github.com/firesim/firesim-riscv-tools-prebuilt.git
+cd firesim-riscv-tools-prebuilt
+./installrelease.sh
+cp -r distrib $RISCV
