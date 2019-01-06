50f79d10fdc9769370a24734faed4ed5c7ec36e4
Jerry Zhao <jerryz123@berkeley.edu>
2019-01-05 18:01:47 -0800

    [rvc] Switch to normal riscv-tools build for CI
    This builds RVC versions of the benchmarks
    boom-template/scripts/build-tools.sh will be deprecated

--- ci/prepare-riscv-tools.sh
+++ ci/prepare-riscv-tools.sh
@@ -13,4 +13,10 @@ rm -rf boom-template
 # clone boom-template and create the riscv-tools
 git clone --progress --verbose https://github.com/riscv-boom/boom-template.git
 cd boom-template
-./scripts/build-tools.sh
+(cd rocket-chip && git submodule update --init riscv-tools)
+(cd rocket-chip/riscv-tools && git submodule update --init --recursive riscv-isa-sim riscv-fesvr riscv-pk riscv-opcodes riscv-tests riscv-gnu-toolchain riscv-openocd)
+
+# build the tools
+cd rocket-chip/riscv-tools
+./build.sh
+
