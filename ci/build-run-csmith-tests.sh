27b8cefe68d932845d39e63b27a410a214353f42
abejgonzalez <abe.j.gonza@gmail.com>
2019-01-15 23:57:29 -0800

    Initial addition of csmith random testing

---
+++ ci/build-run-csmith-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# test the verilator simulation using csmith random testing 
+
+# turn echo on and error on earliest command
+set -x
+set -e
+
+SIM_BASE=simulator-boom.system-
+CONFIG=$1
+SIM=${SIM_BASE}${1}
+AMT_RUNS=$2
+
+# run csmith utility
+cd ci/csmith
+./install-csmith.sh
+./run-csmith.sh --sim $HOME/boom-template/verisim/$SIM --run $AMT_RUNS 
