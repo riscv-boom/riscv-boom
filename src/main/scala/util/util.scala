04193b701a13cf61867cf97d83e32139a76cb562
abejgonzalez <abe.j.gonza@gmail.com>
2019-05-01 09:46:35 -0700

    added conversion functions for fp/x registers

--- src/main/scala/util/util.scala
+++ src/main/scala/util/util.scala
@@ -527,3 +527,47 @@ object RobTypeToChars
     multiVec(rob_type)
   }
 }
+
+object XRegToChars
+{
+  /**
+   * Get a Vec of Strs that can be used for printing
+   *
+   * @param xreg specific register number
+   * @return Vec of Strs (must be indexed to get specific char)
+   */
+  def apply(xreg: UInt) = {
+    val strings = Seq(" x0", " ra", " sp", " gp",
+                      " tp", " t0", " t1", " t2",
+                      " s0", " s1", " a0", " a1",
+                      " a2", " a3", " a4", " a5",
+                      " a6", " a7", " s2", " s3",
+                      " s4", " s5", " s6", " s7",
+                      " s8", " s9", "s10", "s11",
+                      " t3", " t4", " t5", " t6")
+    val multiVec = VecInit(for(string <- strings) yield { VecInit(for (c <- string) yield { Str(c) }) })
+    multiVec(xreg)
+  }
+}
+
+object FPRegToChars
+{
+  /**
+   * Get a Vec of Strs that can be used for printing
+   *
+   * @param fpreg specific register number
+   * @return Vec of Strs (must be indexed to get specific char)
+   */
+  def apply(fpreg: UInt) = {
+    val strings = Seq(" ft0", " ft1", " ft2", " ft3",
+                      " ft4", " ft5", " ft6", " ft7",
+                      " fs0", " fs1", " fa0", " fa1",
+                      " fa2", " fa3", " fa4", " fa5",
+                      " fa6", " fa7", " fs2", " fs3",
+                      " fs4", " fs5", " fs6", " fs7",
+                      " fs8", " fs9", "fs10", "fs11",
+                      " ft8", " ft9", "ft10", "ft11")
+    val multiVec = VecInit(for(string <- strings) yield { VecInit(for (c <- string) yield { Str(c) }) })
+    multiVec(fpreg)
+  }
+}
