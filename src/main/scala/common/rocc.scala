1ec3da489a225914c58f026264ddc32af8c0b834
Ben Korpan <bkorpan@berkeley.edu>
2019-06-09 11:48:07 -0700

    common/rocc style refactor

--- src/main/scala/common/rocc.scala
+++ src/main/scala/common/rocc.scala
@@ -28,13 +28,13 @@ import boom.lsu._
   * Different from normal HasLazyRoCC because we create a new FPU
   */
 trait HasBoomLazyRoCC extends CanHaveBoomPTW { this: BaseTile =>
-   val roccs = p(BuildRoCC).map(_(p))
+  val roccs = p(BuildRoCC).map(_(p))
 
-   roccs.map(_.atlNode).foreach { atl => tlMasterXbar.node :=* atl }
-   roccs.map(_.tlNode).foreach { tl => tlOtherMastersNode :=* tl }
+  roccs.map(_.atlNode).foreach { atl => tlMasterXbar.node :=* atl }
+  roccs.map(_.tlNode).foreach { tl => tlOtherMastersNode :=* tl }
 
-   nPTWPorts += roccs.map(_.nPTWPorts).foldLeft(0)(_+_)
-   nDCachePorts += roccs.size
+  nPTWPorts += roccs.map(_.nPTWPorts).foldLeft(0)(_+_)
+  nDCachePorts += roccs.size
 }
 
 /**
@@ -57,29 +57,28 @@ trait HasBoomLazyRoCCModule extends CanHaveBoomPTWModule
     }
     // Create this FPU just for RoCC
     val nFPUPorts = outer.roccs.filter(_.usesFPU).size
-    if (nFPUPorts > 0)
-    {
-       require(usingFPU)
-       val fpuOpt = outer.tileParams.core.fpu.map(params => Module(new FPU(params)(outer.p)))
-       // TODO: Check this FPU works properly
-       fpuOpt foreach { fpu =>
-          // This FPU does not get CPU requests
-          fpu.io := DontCare
-          fpu.io.fcsr_rm := fcsr_rm
-          fpu.io.dmem_resp_val := false.B
-          fpu.io.valid := false.B
-          fpu.io.killx := false.B
-          fpu.io.killm := false.B
+    if (nFPUPorts > 0) {
+      require(usingFPU)
+      val fpuOpt = outer.tileParams.core.fpu.map(params => Module(new FPU(params)(outer.p)))
+      // TODO: Check this FPU works properly
+      fpuOpt foreach { fpu =>
+        // This FPU does not get CPU requests
+        fpu.io := DontCare
+        fpu.io.fcsr_rm := fcsr_rm
+        fpu.io.dmem_resp_val := false.B
+        fpu.io.valid := false.B
+        fpu.io.killx := false.B
+        fpu.io.killm := false.B
 
-          val fpArb = Module(new InOrderArbiter(new FPInput()(outer.p), new FPResult()(outer.p), nFPUPorts))
-          val fp_rocc_ios = outer.roccs.filter(_.usesFPU).map(_.module.io)
-          fpArb.io.in_req <> fp_rocc_ios.map(_.fpu_req)
-          fp_rocc_ios.zip(fpArb.io.in_resp).foreach {
-             case (rocc, arb) => rocc.fpu_resp <> arb
-          }
-          fpu.io.cp_req <> fpArb.io.out_req
-          fpArb.io.out_resp <> fpu.io.cp_resp
-       }
+        val fpArb = Module(new InOrderArbiter(new FPInput()(outer.p), new FPResult()(outer.p), nFPUPorts))
+        val fp_rocc_ios = outer.roccs.filter(_.usesFPU).map(_.module.io)
+        fpArb.io.in_req <> fp_rocc_ios.map(_.fpu_req)
+        fp_rocc_ios.zip(fpArb.io.in_resp).foreach {
+          case (rocc, arb) => rocc.fpu_resp <> arb
+        }
+        fpu.io.cp_req <> fpArb.io.out_req
+        fpArb.io.out_resp <> fpu.io.cp_resp
+      }
     }
     (Some(respArb), Some(cmdRouter))
   } else {
