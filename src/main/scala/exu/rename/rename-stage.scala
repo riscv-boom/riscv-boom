cbaaa339525afec665456577d70e3b3ea59cf493
Ben Korpan <bkorpan@berkeley.edu>
2019-06-22 16:01:52 -0700

    Re-included and updated copyright headers for rename structures.

--- src/main/scala/exu/rename/rename-stage.scala
+++ src/main/scala/exu/rename/rename-stage.scala
@@ -1,5 +1,5 @@
 //******************************************************************************
-// Copyright (c) 2012 - 2018, The Regents of the University of California (Regents).
+// Copyright (c) 2012 - 2019, The Regents of the University of California (Regents).
 // All Rights Reserved. See LICENSE and LICENSE.SiFive for license details.
 //------------------------------------------------------------------------------
 // Author: Christopher Celio
