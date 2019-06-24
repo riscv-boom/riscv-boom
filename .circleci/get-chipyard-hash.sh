1ded3b8107edd62b271ca522aeab451ffd0d04ac
abejgonzalez <abe.j.gonza@gmail.com>
2019-06-23 22:16:41 -0700

    move hash dir to top dir | cleanup

--- .circleci/get-chipyard-hash.sh
+++ .circleci/get-chipyard-hash.sh
@@ -5,15 +5,16 @@
 # turn echo on and error on earliest command
 set -ex
 
+cd $HOME
+
 # clone chipyard and get its last commit
 git clone --progress --verbose https://github.com/ucb-bar/project-template.git chipyard
 cd chipyard
 git checkout rebar-dev
-
 git rev-parse HEAD > $HOME/chipyard-old-rocket.hash
 
 # use rocket-hash that boom uses in ci (to make bumps easier)
-git submodule update --init $HOME/chipyard/generators/boom
+git submodule update --init generators/boom
 cat $HOME/chipyard-old-rocket.hash $HOME/chipyard/generators/boom/ROCKETCHIP_VERSION >> chipyard.hash
 
 rm -rf $HOME/chipyard
