44bd97c747e566a09445e7b09e34c0a723e23afa
abejgonzalez <abe.j.gonza@gmail.com>
2019-06-24 23:36:10 -0700

    swap to use chipyard branch for ci

--- .circleci/prepare-for-rtl-build.sh
+++ .circleci/prepare-for-rtl-build.sh
@@ -9,8 +9,10 @@ if [ ! -d "$HOME/chipyard" ]; then
     cd $HOME
 
     git clone --progress --verbose https://github.com/ucb-bar/project-template.git chipyard
-    cd chipyard
-    git checkout rebar-dev
+    cd $HOME/chipyard
+
+    echo "Checking out Chipyard version: $(cat $HOME/project/CHIPYARD.hash)"
+    git checkout $(cat $HOME/project/CHIPYARD.hash)
 
     # init all submodules (according to what boom-template wants)
     ./scripts/init-submodules-no-riscv-tools.sh
@@ -18,23 +20,6 @@ if [ ! -d "$HOME/chipyard" ]; then
     # move the pull request riscv-boom repo into boom-template
     rm -rf $HOME/chipyard/generators/boom
     cp -r $HOME/project $HOME/chipyard/generators/boom/
-
-    # get boom specific rocket-chip version
-    echo "Checking out rocket-chip with hash: $(cat boom/ROCKETCHIP_VERSION)"
-    cd generators/rocket-chip
-    git fetch
-    git checkout $(cat $HOME/chipyard/generators/boom/ROCKETCHIP_VERSION)
-
-    echo "Initialize final submodules"
-    git submodule update --init --recursive
-
-    # TODO: remove this
-    # Copy chisel/firrtl of rocketchip into rebar base
-    rm -rf $HOME/chipyard/tools/chisel3
-    rm -rf $HOME/chipyard/tools/firrtl
-    cp -r chisel3 $HOME/chipyard/tools
-    cp -r firrtl $HOME/chipyard/tools
-
 
     # make boom-template verilator version
     cd $HOME/chipyard/sims/verisim
