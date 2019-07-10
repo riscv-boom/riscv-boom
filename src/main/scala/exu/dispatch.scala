92d7b929cef0561eae3f0360726ad5ced13f77a4
Ben Korpan <bkorpan@berkeley.edu>
2019-07-10 11:26:50 -0700

    [dis] Fixed spacing.

--- src/main/scala/exu/dispatch.scala
+++ src/main/scala/exu/dispatch.scala
@@ -24,18 +24,18 @@ import boom.util._
 
 class DispatchIO(implicit p: Parameters) extends BoomBundle
 {
-   // incoming microops from rename2
-   val ren_uops = Vec(coreWidth, Flipped(DecoupledIO(new MicroOp)))
+  // incoming microops from rename2
+  val ren_uops = Vec(coreWidth, Flipped(DecoupledIO(new MicroOp)))
 
-   // outgoing microops to issue queues
-   // N issues each accept up to dispatchWidth uops
-   // dispatchWidth may vary between issue queues
-   val dis_uops = MixedVec(issueParams.map(ip=>Vec(ip.dispatchWidth, DecoupledIO(new MicroOp))))
+  // outgoing microops to issue queues
+  // N issues each accept up to dispatchWidth uops
+  // dispatchWidth may vary between issue queues
+  val dis_uops = MixedVec(issueParams.map(ip=>Vec(ip.dispatchWidth, DecoupledIO(new MicroOp))))
 }
 
 abstract class Dispatcher(implicit p: Parameters) extends BoomModule
 {
-   val io = IO(new DispatchIO)
+  val io = IO(new DispatchIO)
 }
 
 /**
@@ -44,21 +44,20 @@ abstract class Dispatcher(implicit p: Parameters) extends BoomModule
   */
 class BasicDispatcher(implicit p: Parameters) extends Dispatcher
 {
-   issueParams.map(ip=>require(ip.dispatchWidth == coreWidth))
+  issueParams.map(ip=>require(ip.dispatchWidth == coreWidth))
 
-   val ren_readys = io.dis_uops.map(d=>VecInit(d.map(_.ready)).asUInt).reduce(_&_)
+  val ren_readys = io.dis_uops.map(d=>VecInit(d.map(_.ready)).asUInt).reduce(_&_)
 
-   for (w <- 0 until coreWidth)
-   {
-      io.ren_uops(w).ready := ren_readys(w)
-   }
+  for (w <- 0 until coreWidth) {
+    io.ren_uops(w).ready := ren_readys(w)
+  }
 
-   for {i <- 0 until issueParams.size
-        w <- 0 until coreWidth} {
-      val issueParam = issueParams(i)
-      val dis        = io.dis_uops(i)
+  for {i <- 0 until issueParams.size
+       w <- 0 until coreWidth} {
+    val issueParam = issueParams(i)
+    val dis        = io.dis_uops(i)
 
-      dis(w).valid := io.ren_uops(w).valid && ((io.ren_uops(w).bits.iq_type & issueParam.iqType.U) =/= 0.U)
-      dis(w).bits  := io.ren_uops(w).bits
-   }
+    dis(w).valid := io.ren_uops(w).valid && ((io.ren_uops(w).bits.iq_type & issueParam.iqType.U) =/= 0.U)
+    dis(w).bits  := io.ren_uops(w).bits
+  }
 }
