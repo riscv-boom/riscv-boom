12c7f2062afca57a4fc2c3ce1439a12fddb2f81c
Jerry Zhao <jerryz123@berkeley.edu>
2019-07-13 00:22:30 -0700

    [br] Initialize the branch resolution registers
    Avoid sending spurious branch updates.

--- src/main/scala/exu/execution-units/functional-unit.scala
+++ src/main/scala/exu/execution-units/functional-unit.scala
@@ -500,7 +500,7 @@ class ALUUnit(isBranchUnit: Boolean = false, numStages: Int = 1, dataWidth: Int)
     }
 
     val br_unit =
-      if (enableBrResolutionRegister) Reg(new BranchUnitResp)
+      if (enableBrResolutionRegister) RegInit((0.U).asTypeOf(new BranchUnitResp))
       else Wire(new BranchUnitResp)
 
 
@@ -513,7 +513,7 @@ class ALUUnit(isBranchUnit: Boolean = false, numStages: Int = 1, dataWidth: Int)
     // register "brinfo" here, since in that case we would be double counting.
     val brinfo =
       if (enableBrResolutionRegister) Wire(new BrResolutionInfo)
-      else Reg(new BrResolutionInfo)
+      else RegInit((0.U).asTypeOf(new BrResolutionInfo))
 
     // note: jal doesn't allocate a branch-mask, so don't clear a br-mask bit
     brinfo.valid          := io.req.valid && uop.is_br_or_jmp && !uop.is_jal && !killed
