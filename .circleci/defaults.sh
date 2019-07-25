3a06403df71d46f2c42f9baac134a3b2997595e5
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-24 22:52:27 -0700

    fix ci verilator directory

--- .circleci/defaults.sh
+++ .circleci/defaults.sh
@@ -22,14 +22,14 @@ REMOTE_WORK_DIR=$CI_DIR/$CIRCLE_PROJECT_REPONAME-$CIRCLE_BRANCH-$CIRCLE_SHA1-$CI
 REMOTE_RISCV_DIR=$REMOTE_WORK_DIR/riscv-tools-install
 REMOTE_CHIPYARD_DIR=$REMOTE_WORK_DIR/chipyard
 REMOTE_VERILATOR_DIR=$REMOTE_WORK_DIR/verilator
-REMOTE_SIM_DIR=$REMOTE_CHIPYARD_DIR/sims/verisim
+REMOTE_SIM_DIR=$REMOTE_CHIPYARD_DIR/sims/verilator
 
 # local variables (aka within the docker container)
 LOCAL_CHECKOUT_DIR=$HOME/project
 LOCAL_RISCV_DIR=$HOME/riscv-tools-install
 LOCAL_CHIPYARD_DIR=$HOME/chipyard
 LOCAL_VERILATOR_DIR=$HOME/verilator
-LOCAL_SIM_DIR=$LOCAL_CHIPYARD_DIR/sims/verisim
+LOCAL_SIM_DIR=$LOCAL_CHIPYARD_DIR/sims/verilator
 
 # key value store to get the build strings
 declare -A mapping
