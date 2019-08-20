51cb24f7574da799d99be3c38ff80053ed7a3936
Ben Korpan <bkorpan@berkeley.edu>
2019-08-20 15:39:47 -0700

    [lsu] Fix misc. mistakes.

--- src/main/scala/lsu/dcache.scala
+++ src/main/scala/lsu/dcache.scala
@@ -261,12 +261,12 @@ class BoomL1MetaReadReq(implicit p: Parameters) extends BoomBundle()(p) {
 }
 
 class BoomDataArray(implicit p: Parameters) extends BoomModule with HasL1HellaCacheParameters {
-  val io = new Bundle {
+  val io = IO(new BoomBundle {
     val read  = Vec(memWidth, Flipped(Decoupled(new L1DataReadReq)))
     val write = Flipped(Decoupled(new L1DataWriteReq))
     val resp  = Output(Vec(memWidth, Vec(nWays, Bits(encRowBits.W))))
     val nacks = Output(Vec(memWidth, Bool()))
-  }
+  })
 
   def pipeMap[T <: Data](f: Int => T) = VecInit((0 until memWidth).map(f))
 
@@ -276,21 +276,21 @@ class BoomDataArray(implicit p: Parameters) extends BoomModule with HasL1HellaCa
   require (bankSize > 0)
 
   val bankBits    = log2Ceil(nBanks)
-  val bankOffBits = log2Ceil(rowWords)
-  val idxBits     = log2Ceil(bankSize)
-  val idxOffBits  = bankOffBits + bankBits
+  val bankOffBits = log2Ceil(rowWords) + log2Ceil(wordBytes)
+  val bidxBits    = log2Ceil(bankSize)
+  val bidxOffBits = bankOffBits + bankBits
 
   //----------------------------------------------------------------------------------------------------
 
-  val s0_rbanks = if (nBanks > 1) VecInit(io.read.map((_.bits.addr >> bankOffBits)(bankBits-1,0))) else Vec(1, 0.U)
+  val s0_rbanks = if (nBanks > 1) VecInit(io.read.map(r => (r.bits.addr >> bankOffBits)(bankBits-1,0))) else VecInit(0.U)
   val s0_wbank  = if (nBanks > 1) (io.write.bits.addr >> bankOffBits)(bankBits-1,0) else 0.U
-  val s0_ridxs  = VecInit(io.read.map((_.bits.addr >> idxOffBits)(idxBits-1,0)))
-  val s0_widx   = (io.write.bits.addr >> idxOffBits)(idxBits-1,0)
+  val s0_ridxs  = VecInit(io.read.map(r => (r.bits.addr >> bidxOffBits)(bidxBits-1,0)))
+  val s0_widx   = (io.write.bits.addr >> bidxOffBits)(bidxBits-1,0)
 
   val s0_read_valids    = VecInit(io.read.map(_.valid))
-  val s0_bank_conflicts = pipeMap(w => (0 until w).foldLeft(false.B)(i => io.read(i).valid && rbank(i) === rbank(w)))
+  val s0_bank_conflicts = pipeMap(w => (0 until w).foldLeft(false.B)((c,i) => c || io.read(i).valid && s0_rbanks(i) === s0_rbanks(w)))
   val s0_do_bank_read   = s0_read_valids zip s0_bank_conflicts map {case (v,c) => v && !c}
-  val s0_bank_read_gnts = Transpose(s0_rbanks zip s0_do_bank_read map {case (b,d) => (UIntToOH(b) & Fill(nBanks,d)).asBools})
+  val s0_bank_read_gnts = Transpose(VecInit(s0_rbanks zip s0_do_bank_read map {case (b,d) => VecInit((UIntToOH(b) & Fill(nBanks,d)).asBools)}))
   val s0_bank_write_gnt = (UIntToOH(s0_wbank) & Fill(nBanks, io.write.valid)).asBools
 
   //----------------------------------------------------------------------------------------------------
@@ -298,11 +298,13 @@ class BoomDataArray(implicit p: Parameters) extends BoomModule with HasL1HellaCa
   val s1_rbanks         = RegNext(s0_rbanks)
   val s1_ridxs          = RegNext(s0_ridxs)
   val s1_read_valids    = RegNext(s0_read_valids)
-  val s1_bank_conflicts = RegNext(s0_bank_conflicts)
-  val s1_bank_selection = pipeMap(w => PriorityEncoderOH((0 to w).map(i =>
-                            if (i == w) true.B else s1_read_valids(i) && s1_rbanks(i) === s1_rbanks(w))))
-  val s1_ridx_match     = pipeMap(w => (0 to w).map(i => if (i == w) true.B else s1_ridxs(i) === s1_ridxs(w)))
-  val s1_nacks          = VecInit((s1_bank_selection zip s1_ridx_match) map {case (s,m) => (s.asUInt & ~m.asUInt).orR})
+  val s1_pipe_selection = pipeMap(i => VecInit(PriorityEncoderOH(pipeMap(j =>
+                            if (j < i) s1_read_valids(j) && s1_rbanks(j) === s1_rbanks(i)
+                            else if (j == i) true.B else false.B))))
+  val s1_ridx_match     = pipeMap(i => pipeMap(j => if (j < i) s1_ridxs(j) === s1_ridxs(i)
+                                                    else if (j == i) true.B else false.B))
+  val s1_nacks          = pipeMap(w => s1_read_valids(w) && (s1_pipe_selection(w).asUInt & ~s1_ridx_match(w).asUInt).orR)
+  val s1_bank_selection = pipeMap(w => Mux1H(s1_pipe_selection(w), s1_rbanks))
 
   //----------------------------------------------------------------------------------------------------
 
@@ -310,7 +312,7 @@ class BoomDataArray(implicit p: Parameters) extends BoomModule with HasL1HellaCa
   val s2_nacks          = RegNext(s1_nacks)
 
   for (w <- 0 until nWays) {
-    val s2_bank_reads = Reg(Vec(nBanks, Bits(encDataBits.W)))
+    val s2_bank_reads = Reg(Vec(nBanks, Bits(encRowBits.W)))
 
     for (b <- 0 until nBanks) {
       val (array, omSRAM) = DescribedSRAM(
@@ -322,20 +324,22 @@ class BoomDataArray(implicit p: Parameters) extends BoomModule with HasL1HellaCa
 
       val ridx = Mux1H(s0_bank_read_gnts(b), s0_ridxs)
       val way_en = Mux1H(s0_bank_read_gnts(b), io.read.map(_.bits.way_en))
-      s1_bank_reads(b) := array.read(ridx, way_en && s0_bank_read_gnts(b))
+      s2_bank_reads(b) := array.read(ridx, way_en(w) && s0_bank_read_gnts(b).reduce(_||_)).asUInt
 
       when (io.write.bits.way_en(w) && s0_bank_write_gnt(b)) {
-        val data = Vec.tabulate(rowWords)(i => io.write.bits.data(encDataBits*(i+1)-1,encDataBits*i))
+        val data = VecInit.tabulate(rowWords)(i => io.write.bits.data(encDataBits*(i+1)-1,encDataBits*i))
         array.write(s0_widx, data, io.write.bits.wmask.asBools)
       }
     }
 
-    io.resp(w) := Mux1H(s2_bank_selection, s2_bank_reads)
+    for (i <- 0 until memWidth) {
+      io.resp(i)(w) := s2_bank_reads(s2_bank_selection(i))
+    }
   }
 
   io.nacks := s2_nacks
 
-  io.read.ready  := true.B
+  (0 until memWidth) foreach (w => io.read(w).ready := true.B)
   io.write.ready := true.B
 }
 
@@ -429,31 +433,31 @@ class BoomNonBlockingDCacheModule(outer: BoomNonBlockingDCache) extends LazyModu
   // 0 goes to pipeline, 1 goes to MSHR refills
   val dataReadArb = Module(new Arbiter(new BoomL1DataReadReq, 3))
   // 0 goes to MSHR replays, 1 goes to wb, 2 goes to pipeline
-   dataReadArb.io.in := DontCare
+  dataReadArb.io.in := DontCare
 
   for (w <- 0 until memWidth) {
-    data.io.read(w).valid := dataReadArb.io.out.bits.valid(w)
+    data.io.read(w).valid := dataReadArb.io.out.bits.valid(w) && dataReadArb.io.out.valid
     data.io.read(w).bits  := dataReadArb.io.out.bits.req(w)
   }
-  dataReadArb.io.out.ready := data.map(_.io.read.ready).reduce(_||_)
+  dataReadArb.io.out.ready := true.B
 
-  data(w).io.write.valid := dataWriteArb.io.out.fire()
-  data(w).io.write.bits  := dataWriteArb.io.out.bits
-  dataWriteArb.io.out.ready := data.map(_.io.write.ready).reduce(_||_)
+  data.io.write.valid := dataWriteArb.io.out.fire()
+  data.io.write.bits  := dataWriteArb.io.out.bits
+  dataWriteArb.io.out.ready := true.B
 
   // ------------
   // New requests
 
   io.lsu.req.ready := metaReadArb.io.in(4).ready && dataReadArb.io.in(2).ready
-  metaReadArb.io.in(4).valid       := io.lsu.req.valid
-  dataReadArb.io.in(2).valid       := io.lsu.req.valid
+  metaReadArb.io.in(4).valid := io.lsu.req.valid
+  dataReadArb.io.in(2).valid := io.lsu.req.valid
   for (w <- 0 until memWidth) {
     // Tag read for new requests
     metaReadArb.io.in(4).bits.req(w).idx    := io.lsu.req.bits(w).bits.addr >> blockOffBits
     metaReadArb.io.in(4).bits.req(w).way_en := DontCare
     metaReadArb.io.in(4).bits.req(w).tag    := DontCare
     // Data read for new requests
-    dataReadArb.io.in(2).valid              := io.lsu.req.bits(w).valid
+    dataReadArb.io.in(2).bits.valid(w)      := io.lsu.req.bits(w).valid
     dataReadArb.io.in(2).bits.req(w).addr   := io.lsu.req.bits(w).bits.addr
     dataReadArb.io.in(2).bits.req(w).way_en := ~0.U(nWays.W)
   }
@@ -477,7 +481,8 @@ class BoomNonBlockingDCacheModule(outer: BoomNonBlockingDCache) extends LazyModu
   dataReadArb.io.in(0).valid              := mshrs.io.replay.valid
   dataReadArb.io.in(0).bits.req(0).addr   := mshrs.io.replay.bits.addr
   dataReadArb.io.in(0).bits.req(0).way_en := mshrs.io.replay.bits.way_en
-  dataReadArb.io.in(0).bits.valid         := widthMap(w => (w ==  0).B)
+  dataReadArb.io.in(0).bits.valid         := widthMap(w => (w == 0).B)
+
   // -----------
   // MSHR Meta read
   val mshr_read_req = Wire(Vec(memWidth, new BoomDCacheReq))
@@ -694,7 +699,7 @@ class BoomNonBlockingDCacheModule(outer: BoomNonBlockingDCache) extends LazyModu
   val s2_nack_victim = widthMap(w => s2_valid(w) &&  s2_hit(w) && mshrs.io.secondary_miss(w))
   // MSHRs not ready for request
   val s2_nack_miss   = widthMap(w => s2_valid(w) && !s2_hit(w) && !mshrs.io.req(w).ready)
-  s2_nack           := widthMap(w => (s2_nack_miss(w) || s2_nack_hit(w) || s2_nack_victim(w)) && s2_type =/= t_replay || data.io.nacks(w))
+  s2_nack           := widthMap(w => (s2_nack_miss(w) || s2_nack_hit(w) || s2_nack_victim(w) || data.io.nacks(w)) && s2_type =/= t_replay)
   val s2_send_resp = widthMap(w => (RegNext(s1_send_resp_or_nack(w)) && !s2_nack(w) &&
                       (s2_hit(w) || (mshrs.io.req(w).fire() && isWrite(s2_req(w).uop.mem_cmd) && !isRead(s2_req(w).uop.mem_cmd)))))
   val s2_send_nack = widthMap(w => (RegNext(s1_send_resp_or_nack(w)) && s2_nack(w)))
