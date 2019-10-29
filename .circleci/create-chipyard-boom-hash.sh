88c8e45f5471aa929c02c538958c2690936a5074
abejgonzalez <abe.j.gonza@gmail.com>
2019-10-29 11:55:19 -0700

    retrieve cache for run workloads | get defaults in hash gen script

--- .circleci/create-chipyard-boom-hash.sh
+++ .circleci/create-chipyard-boom-hash.sh
@@ -5,4 +5,8 @@
 # turn echo on and error on earliest command
 set -ex
 
+# get shared variables
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+source $SCRIPT_DIR/defaults.sh
+
 cat $LOCAL_CHECKOUT_DIR/CHIPYARD.hash $CIRCLE_SHA1 &> $HOME/chipyard-boom.hash
