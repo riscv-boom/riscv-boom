aa39334d4a24ac2266de5974f2733fa72fca380c
abejgonzalez <abe.j.gonza@gmail.com>
2019-11-03 12:20:03 -0800

    reorganize files | more variables | cleaner comments

--- .circleci/defaults.sh
+++ .circleci/defaults.sh
@@ -121,6 +121,7 @@ LOCAL_ESP_DIR=$HOME/esp-tools-install
 LOCAL_CHIPYARD_DIR=$HOME/chipyard
 LOCAL_VERILATOR_DIR=$HOME/verilator
 LOCAL_SIM_DIR=$LOCAL_CHIPYARD_DIR/sims/verilator
+LOCAL_FSIM_CFGS_DIR=$LOCAL_CHECKOUT_DIR/.circleci/firesim-configs
 
 # api url to do curls
 API_URL=https://circleci.com/api/v1.1
--- .circleci/firesim-build-largeboom-afi.sh
+++ .circleci/firesim-build-largeboom-afi.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# make an afi and detach from server
-# spawn all jobs related to it
-# requires workloads to be done before this afi is built
-# usage
-#   $1 - the name of the afi to build
+# -------------------------------------------------------------
+# make an afi (run, detach so it runs in the background)
+# spawn all jobs related to it (specific to this script)
+# **runs all workloads that finished building**
+#   - otherwise it just skips that workload
+#
+# usage:
+#   $1 - firesim afi longname (folder inside firesim-configs/*)
+#-------------------------------------------------------------
 
 # turn echo on and error on earliest command
 set -ex
@@ -13,17 +17,23 @@ set -ex
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source $SCRIPT_DIR/defaults.sh
 
+# setup arguments
+AFI_NAME=$1
+
 # set stricthostkeychecking to no (must happen before rsync)
 run_aws "echo \"Ping $AWS_SERVER\""
 
-copy $LOCAL_CHECKOUT_DIR/.circleci/firesim-configs/$1 $AWS_SERVER:$REMOTE_AWS_FSIM_DEPLOY_DIR/
+# copy over the configuration collateral
+copy $LOCAL_FSIM_CFGS_DIR/$AFI_NAME $AWS_SERVER:$REMOTE_AWS_FSIM_DEPLOY_DIR/
 
 # setup workload variables
-BUILDROOT_CFG=$LOCAL_CHECKOUT_DIR/.circleci/firesim-configs/buildroot/firemarshal_config
-FEDORA_CFG=$LOCAL_CHECKOUT_DIR/.circleci/firesim-configs/fedora/firemarshal_config
+BUILDROOT_CFG=$LOCAL_FSIM_CFGS_DIR/$AFI_NAME/buildroot/firemarshal_config
+FEDORA_CFG=$LOCAL_FSIM_CFGS_DIR/$AFI_NAME/fedora/firemarshal_config
+
+SCRIPT_NAME=firesim-build-$AFI_NAME-afi.sh
 
 # create a script to run
-cat <<EOF >> $LOCAL_CHECKOUT_DIR/firesim-build-$1-afi.sh
+cat <<EOF >> $LOCAL_CHECKOUT_DIR/$SCRIPT_NAME
 #!/bin/bash
 
 set -ex
@@ -35,7 +45,7 @@ source sourceme-f1-manager.sh
 set +e
 
 # build afi
-if firesim buildafi -b $REMOTE_AWS_FSIM_DEPLOY_DIR/$1/config_build.ini -r $REMOTE_AWS_FSIM_DEPLOY_DIR/$1/config_build_recipes.ini; then
+if firesim buildafi -b $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build.ini -r $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build_recipes.ini; then
     echo "AFI successfully built"
 else
     # spawn fail job
@@ -70,8 +80,8 @@ fi
 EOF
 
 echo "script created"
-cat $LOCAL_CHECKOUT_DIR/firesim-build-$1-afi.sh
+cat $LOCAL_CHECKOUT_DIR/$SCRIPT_NAME
 
 # execute the script and detach
-chmod +x $LOCAL_CHECKOUT_DIR/firesim-build-$1-afi.sh
-run_detach_script_aws $1 $LOCAL_CHECKOUT_DIR firesim-build-$1-afi.sh
+chmod +x $LOCAL_CHECKOUT_DIR/$SCRIPT_NAME
+run_detach_script_aws $AFI_NAME $LOCAL_CHECKOUT_DIR $SCRIPT_NAME
