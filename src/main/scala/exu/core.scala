9c0af7943fc455dc0205f444d90f079a70d09f7c
Jerry Zhao <jerryz123@berkeley.edu>
2019-11-13 15:40:09 -0800

    [exu] Push interrupts through decode even when frontend has stalled

--- src/main/scala/exu/core.scala
+++ src/main/scala/exu/core.scala
@@ -395,7 +395,7 @@ class BoomCore(implicit p: Parameters) extends BoomModule
   val branch_mask_full = Wire(Vec(coreWidth, Bool()))
 
   for (w <- 0 until coreWidth) {
-    dec_valids(w)                      := io.ifu.fetchpacket.valid && dec_fbundle.uops(w).valid &&
+    dec_valids(w)                      := ((io.ifu.fetchpacket.valid && dec_fbundle.uops(w).valid) || csr.io.interrupt) &&
                                           !dec_finished_mask(w)
     decode_units(w).io.enq.uop         := dec_fbundle.uops(w).bits
     decode_units(w).io.status          := csr.io.status
