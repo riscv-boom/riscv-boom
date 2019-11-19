da6836aa2cc447999a904550c5d6a5cce9698e06
abejgonzalez <abe.j.gonza@gmail.com>
2019-11-19 12:00:25 -0800

    test: use older afi

--- .circleci/firesim-build-afi.sh
+++ .circleci/firesim-build-afi.sh
@@ -45,18 +45,19 @@ source sourceme-f1-manager.sh
 
 set +e
 
-# build afi
-if firesim buildafi -b $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build.ini -r $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build_recipes.ini; then
-    echo "AFI successfully built"
-else
-    # spawn fail job
-    echo "AFI failed... spawning failure job"
-    curl -u $API_TOKEN: \
-        -d build_parameters[CIRCLE_JOB]=$CONFIG_KEY-afi-failed \
-        -d revision=$CIRCLE_SHA1 \
-        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
-    exit 1
-fi
+# TODO: RE-ENABLE
+## build afi
+#if firesim buildafi -b $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build.ini -r $REMOTE_AWS_FSIM_DEPLOY_DIR/$AFI_NAME/config_build_recipes.ini; then
+#    echo "AFI successfully built"
+#else
+#    # spawn fail job
+#    echo "AFI failed... spawning failure job"
+#    curl -u $API_TOKEN: \
+#        -d build_parameters[CIRCLE_JOB]=$CONFIG_KEY-afi-failed \
+#        -d revision=$CIRCLE_SHA1 \
+#        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+#    exit 1
+#fi
 
 # launch workloads related to this afi
 cd $REMOTE_AWS_FSIM_DEPLOY_DIR
