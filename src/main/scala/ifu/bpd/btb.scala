bd5d25d627aea08a23444c3b4c9d2da04e85e0f5
Jerry Zhao <jerryz123@berkeley.edu>
2020-01-24 11:35:23 -0800

    [bpd] Enable set-associative backing BTB

--- src/main/scala/ifu/bpd/btb.scala
+++ src/main/scala/ifu/bpd/btb.scala
@@ -11,25 +11,29 @@ import freechips.rocketchip.tilelink._
 import boom.common._
 import boom.util.{BoomCoreStringPrefix}
 
+import scala.math.min
+
 case class BoomBTBParams(
-  nSets: Int = 256,
+  nSets: Int = 128,
+  nWays: Int = 2,
   offsetSz: Int = 13,
-  extendedNSets: Int = 256,
-  bimNSets: Int = 1024
+  extendedNSets: Int = 32,
+  bimNSets: Int = 2048
 )
 
 
 class BTBBranchPredictorBank(params: BoomBTBParams)(implicit p: Parameters) extends BranchPredictorBank()(p)
 {
   override val nSets         = params.nSets
+  override val nWays         = params.nWays
   val tagSz         = vaddrBitsExtended - log2Ceil(nSets) - log2Ceil(fetchWidth) - 1
   val offsetSz      = params.offsetSz
   val extendedNSets = params.extendedNSets
-  val useEBTB       = extendedNSets != 0
 
   require(isPow2(nSets))
   require(isPow2(extendedNSets) || extendedNSets == 0)
   require(extendedNSets <= nSets)
+  require(extendedNSets >= 1)
   require(nSets <= params.bimNSets)
 
   class BTBEntry extends Bundle {
@@ -44,7 +48,12 @@ class BTBBranchPredictorBank(params: BoomBTBParams)(implicit p: Parameters) exte
   }
   val btbMetaSz = tagSz + 1
 
+  class BTBPredictMeta extends Bundle {
+    val write_way = UInt(log2Ceil(nWays).W)
+  }
 
+  val s1_meta = Wire(new BTBPredictMeta)
+  val f3_meta = RegNext(RegNext(s1_meta))
 
 
   val bim = Module(new BIMBranchPredictorBank(params.bimNSets))
@@ -54,60 +63,75 @@ class BTBBranchPredictorBank(params: BoomBTBParams)(implicit p: Parameters) exte
 
   bim.io.f0_req := io.f0_req
   bim.io.update := io.update
-  io.f1_resp := bim.io.f1_resp
-  io.f2_resp := bim.io.f2_resp
-  io.f3_resp := bim.io.f3_resp
-  io.f3_meta := bim.io.f3_meta
+  io.f3_meta := Cat(f3_meta.asUInt, bim.io.f3_meta(bim.metaSz-1,0))
 
-  override val metaSz = bim.metaSz
+  override val metaSz = bim.metaSz + s1_meta.asUInt.getWidth
 
   val doing_reset = RegInit(true.B)
   val reset_idx   = RegInit(0.U(log2Ceil(nSets).W))
   reset_idx := reset_idx + doing_reset
   when (reset_idx === (nSets-1).U) { doing_reset := false.B }
 
-  val meta     = SyncReadMem(nSets, Vec(bankWidth, UInt(btbMetaSz.W)))
-  val btb      = SyncReadMem(nSets, Vec(bankWidth, UInt(btbEntrySz.W)))
-  val ebtb     = SyncReadMem(extendedNSets max 1, UInt(vaddrBitsExtended.W))
+  val meta     = Seq.fill(nWays) { SyncReadMem(nSets, Vec(bankWidth, UInt(btbMetaSz.W))) }
+  val btb      = Seq.fill(nWays) { SyncReadMem(nSets, Vec(bankWidth, UInt(btbEntrySz.W))) }
+  val ebtb     = SyncReadMem(extendedNSets, UInt(vaddrBitsExtended.W))
 
-  val s1_req_rbtb  = VecInit(btb.read(s0_req_idx , io.f0_req.valid).map(_.asTypeOf(new BTBEntry)))
-  val s1_req_rmeta = VecInit(meta.read(s0_req_idx, io.f0_req.valid).map(_.asTypeOf(new BTBMeta)))
+  val s1_req_rbtb  = VecInit(btb.map { b => VecInit(b.read(s0_req_idx , io.f0_req.valid).map(_.asTypeOf(new BTBEntry))) })
+  val s1_req_rmeta = VecInit(meta.map { m => VecInit(m.read(s0_req_idx, io.f0_req.valid).map(_.asTypeOf(new BTBMeta))) })
   val s1_req_rebtb = ebtb.read(s0_req_idx, io.f0_req.valid)
   val s1_req_tag   = s1_req_idx >> log2Ceil(nSets)
-
 
   val s1_resp   = Wire(Vec(bankWidth, Valid(UInt(vaddrBitsExtended.W))))
   val s1_is_br  = Wire(Vec(bankWidth, Bool()))
   val s1_is_jal = Wire(Vec(bankWidth, Bool()))
 
+  val s1_hit_ohs = VecInit((0 until bankWidth) map { i =>
+    VecInit((0 until nWays) map { w =>
+      s1_req_rmeta(w)(i).tag === s1_req_tag(tagSz-1,0)
+    })
+  })
+  val s1_hits     = s1_hit_ohs.map { oh => oh.reduce(_||_) }
+  val s1_hit_ways = s1_hit_ohs.map { oh => PriorityEncoder(oh) }
+
   for (w <- 0 until bankWidth) {
-    s1_resp(w).valid := !doing_reset && s1_req.valid && s1_req_tag(tagSz-1,0) === s1_req_rmeta(w).tag
+    val entry_meta = s1_req_rmeta(s1_hit_ways(w))(w)
+    val entry_btb  = s1_req_rbtb(s1_hit_ways(w))(w)
+    s1_resp(w).valid := !doing_reset && s1_req.valid && s1_hits(w)
     s1_resp(w).bits  := Mux(
-      s1_req_rbtb(w).extended && useEBTB.B,
+      entry_btb.extended,
       s1_req_rebtb,
-      (s1_req.bits.pc.asSInt + (w << 1).S + s1_req_rbtb(w).offset).asUInt)
-    s1_is_br(w)  := !doing_reset && s1_resp(w).valid &&  s1_req_rmeta(w).is_br
-    s1_is_jal(w) := !doing_reset && s1_resp(w).valid && !s1_req_rmeta(w).is_br
+      (s1_req.bits.pc.asSInt + (w << 1).S + entry_btb.offset).asUInt)
+    s1_is_br(w)  := !doing_reset && s1_resp(w).valid &&  entry_meta.is_br
+    s1_is_jal(w) := !doing_reset && s1_resp(w).valid && !entry_meta.is_br
 
     io.f2_resp(w).predicted_pc := RegNext(s1_resp(w))
     io.f2_resp(w).is_br        := RegNext(s1_is_br(w))
     io.f2_resp(w).is_jal       := RegNext(s1_is_jal(w))
-    when (RegNext(s1_is_jal(w))) {
-      io.f2_resp(w).taken := true.B
-    }
+    io.f2_resp(w).taken        := RegNext(s1_is_jal(w)) || bim.io.f2_resp(w).taken
 
-    io.f3_resp(w).predicted_pc := RegNext(RegNext(s1_resp(w)))
-    io.f3_resp(w).is_br        := RegNext(RegNext(s1_is_br(w)))
-    io.f3_resp(w).is_jal       := RegNext(RegNext(s1_is_jal(w)))
-    when (RegNext(RegNext(s1_is_jal(w)))) {
-      io.f3_resp(w).taken := true.B
-    }
+    io.f3_resp(w)              := RegNext(io.f2_resp(w))
   }
 
-  val s1_update_cfi_idx = s1_update.bits.cfi_idx.bits
+  val alloc_way = if (nWays > 1) {
+    val r_metas = Cat(VecInit(s1_req_rmeta.map { w => VecInit(w.map(_.tag)) }).asUInt, s1_req_tag(tagSz-1,0))
+    val l = log2Ceil(nWays)
+    val nChunks = (r_metas.getWidth + l - 1) / l
+    val chunks = (0 until nChunks) map { i =>
+      r_metas(min((i+1)*l, r_metas.getWidth)-1, i*l)
+    }
+    chunks.reduce(_^_)
+  } else {
+    0.U
+  }
+  s1_meta.write_way := Mux(s1_hits.reduce(_||_),
+    PriorityEncoder(s1_hit_ohs.map(_.asUInt).reduce(_|_)),
+    alloc_way)
 
-  val max_offset_value = (~(0.U)((offsetSz-1).W)).asSInt
-  val min_offset_value = Cat(1.B, (0.U)((offsetSz-1).W)).asSInt
+  val s1_update_cfi_idx = s1_update.bits.cfi_idx.bits
+  val s1_update_meta    = (s1_update.bits.meta >> bim.metaSz).asTypeOf(new BTBPredictMeta)
+
+  val max_offset_value = Cat(0.B, ~(0.U((offsetSz-1).W))).asSInt
+  val min_offset_value = Cat(1.B,  (0.U((offsetSz-1).W))).asSInt
   val new_offset_value = (s1_update.bits.target.asSInt -
     (s1_update.bits.pc + (s1_update.bits.cfi_idx.bits << 1)).asSInt)
   val offset_is_extended = (new_offset_value > max_offset_value ||
@@ -128,34 +152,37 @@ class BTBBranchPredictorBank(params: BoomBTBParams)(implicit p: Parameters) exte
     s1_update_wmeta_data(w).is_br   := s1_update.bits.br_mask(w)
   }
 
-  btb.write(
-    Mux(doing_reset,
-      reset_idx,
-      s1_update_idx),
-    Mux(doing_reset,
-      VecInit(Seq.fill(bankWidth) { 0.U(btbEntrySz.W) }),
-      VecInit(Seq.fill(bankWidth) { s1_update_wbtb_data.asUInt })),
-    Mux(doing_reset,
-      (~(0.U(bankWidth.W))),
-      s1_update_wbtb_mask).asBools
-  )
-  meta.write(
-    Mux(doing_reset,
-      reset_idx,
-      s1_update_idx),
-    Mux(doing_reset,
-      VecInit(Seq.fill(bankWidth) { 0.U(btbMetaSz.W) }),
-      VecInit(s1_update_wmeta_data.map(_.asUInt))),
-    Mux(doing_reset,
-      (~(0.U(bankWidth.W))),
-      s1_update_wmeta_mask).asBools
-  )
+  for (w <- 0 until nWays) {
+    when (doing_reset || s1_update_meta.write_way === w.U || (w == 0 && nWays == 1).B) {
+      btb(w).write(
+        Mux(doing_reset,
+          reset_idx,
+          s1_update_idx),
+        Mux(doing_reset,
+          VecInit(Seq.fill(bankWidth) { 0.U(btbEntrySz.W) }),
+          VecInit(Seq.fill(bankWidth) { s1_update_wbtb_data.asUInt })),
+        Mux(doing_reset,
+          (~(0.U(bankWidth.W))),
+          s1_update_wbtb_mask).asBools
+      )
+      meta(w).write(
+        Mux(doing_reset,
+          reset_idx,
+          s1_update_idx),
+        Mux(doing_reset,
+          VecInit(Seq.fill(bankWidth) { 0.U(btbMetaSz.W) }),
+          VecInit(s1_update_wmeta_data.map(_.asUInt))),
+        Mux(doing_reset,
+          (~(0.U(bankWidth.W))),
+          s1_update_wmeta_mask).asBools
+      )
 
 
-  if (useEBTB) {
-    when (s1_update_wbtb_mask =/= 0.U && offset_is_extended) {
-      ebtb.write(s1_update_idx, s1_update.bits.target)
     }
   }
+  when (s1_update_wbtb_mask =/= 0.U && offset_is_extended) {
+    ebtb.write(s1_update_idx, s1_update.bits.target)
+  }
+
 }
 
