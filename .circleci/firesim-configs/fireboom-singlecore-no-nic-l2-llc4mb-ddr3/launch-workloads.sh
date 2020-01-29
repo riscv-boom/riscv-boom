f8aec26750459254b35bf85f1b10121e653c2075
abejgonzalez <abe.j.gonza@gmail.com>
2020-01-29 09:36:03 -0800

    disable spec17 runs

--- .circleci/firesim-configs/fireboom-singlecore-no-nic-l2-llc4mb-ddr3/launch-workloads.sh
+++ .circleci/firesim-configs/fireboom-singlecore-no-nic-l2-llc4mb-ddr3/launch-workloads.sh
@@ -67,28 +67,29 @@ if [ -f $REMOTE_AWS_WORK_DIR/$AFI_NAME-$WORKLOAD_NAME-FINISHED ]; then
     echo "$AFI_NAME-$WORKLOAD_NAME" >> $REMOTE_AWS_WORK_DIR/workloads_running
 fi
 
+# TODO: Re-enable
 # run workload 4 - spec17-intspeed
-WORKLOAD_NAME=spec17-intspeed
-INTSPEED_CFG=$DEPLOY_DIR/$AFI_NAME/$WORKLOAD_NAME/firemarshal_config
-FMRSHL_NAME=$(sed -n '2p' $INTSPEED_CFG)
-FMRSHL_DIR_NAME=$(sed -n '1p' $INTSPEED_CFG)
-if [ -f $REMOTE_AWS_WORK_DIR/$AFI_NAME-$WORKLOAD_NAME-FINISHED ]; then
-    curl -u $API_TOKEN: \
-        -d build_parameters[CIRCLE_JOB]=launch-$CONFIG_KEY-$WORKLOAD_NAME-run \
-        -d revision=$CIRCLE_SHA1 \
-        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
-    echo "$AFI_NAME-$WORKLOAD_NAME" >> $REMOTE_AWS_WORK_DIR/workloads_running
-fi
-
-# run workload 5 - spec17-intrate
-WORKLOAD_NAME=spec17-intrate
-INTRATE_CFG=$DEPLOY_DIR/$AFI_NAME/$WORKLOAD_NAME/firemarshal_config
-FMRSHL_NAME=$(sed -n '2p' $INTRATE_CFG)
-FMRSHL_DIR_NAME=$(sed -n '1p' $INTRATE_CFG)
-if [ -f $REMOTE_AWS_WORK_DIR/$AFI_NAME-$WORKLOAD_NAME-FINISHED ]; then
-    curl -u $API_TOKEN: \
-        -d build_parameters[CIRCLE_JOB]=launch-$CONFIG_KEY-$WORKLOAD_NAME-run \
-        -d revision=$CIRCLE_SHA1 \
-        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
-    echo "$AFI_NAME-$WORKLOAD_NAME" >> $REMOTE_AWS_WORK_DIR/workloads_running
-fi
+#WORKLOAD_NAME=spec17-intspeed
+#INTSPEED_CFG=$DEPLOY_DIR/$AFI_NAME/$WORKLOAD_NAME/firemarshal_config
+#FMRSHL_NAME=$(sed -n '2p' $INTSPEED_CFG)
+#FMRSHL_DIR_NAME=$(sed -n '1p' $INTSPEED_CFG)
+#if [ -f $REMOTE_AWS_WORK_DIR/$AFI_NAME-$WORKLOAD_NAME-FINISHED ]; then
+#    curl -u $API_TOKEN: \
+#        -d build_parameters[CIRCLE_JOB]=launch-$CONFIG_KEY-$WORKLOAD_NAME-run \
+#        -d revision=$CIRCLE_SHA1 \
+#        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+#    echo "$AFI_NAME-$WORKLOAD_NAME" >> $REMOTE_AWS_WORK_DIR/workloads_running
+#fi
+#
+## run workload 5 - spec17-intrate
+#WORKLOAD_NAME=spec17-intrate
+#INTRATE_CFG=$DEPLOY_DIR/$AFI_NAME/$WORKLOAD_NAME/firemarshal_config
+#FMRSHL_NAME=$(sed -n '2p' $INTRATE_CFG)
+#FMRSHL_DIR_NAME=$(sed -n '1p' $INTRATE_CFG)
+#if [ -f $REMOTE_AWS_WORK_DIR/$AFI_NAME-$WORKLOAD_NAME-FINISHED ]; then
+#    curl -u $API_TOKEN: \
+#        -d build_parameters[CIRCLE_JOB]=launch-$CONFIG_KEY-$WORKLOAD_NAME-run \
+#        -d revision=$CIRCLE_SHA1 \
+#        $API_URL/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+#    echo "$AFI_NAME-$WORKLOAD_NAME" >> $REMOTE_AWS_WORK_DIR/workloads_running
+#fi
