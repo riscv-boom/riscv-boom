GaloisInc/BESSPIN-riscv-boom

cleanup + first test (add click to continue guards)

.circleci/defaults.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
+# shared variables between the different services
+#
+# CircleCI set values:
+#   $SERVER - points to the millennium build server
+#   $AWS_SERVER - points to the aws manager instance
+#   $CI_DIR - home directory on build server
+#   $CI_AWS_DIR - home directory on aws
+
+##################
+# SHARED FUNCTIONS
+##################
+
+# shared across build server and aws
+
 copy () {
     rsync -avzp -e 'ssh' $1 $2
 }
 
+run_impl () {
+    ssh -o "StrictHostKeyChecking no" -t $1 $2
+}
+
+run_script_impl () {
+    ssh -o "StrictHostKeyChecking no" -t $1 'bash -s' < $2 "$3"
+}
+
+# build server calls
+
 run () {
-    ssh -o "StrictHostKeyChecking no" -t $SERVER $@
+    run_impl $SERVER $@
 }
 
 run_script () {
-    ssh -o "StrictHostKeyChecking no" -t $SERVER 'bash -s' < $1 "$2"
+    run_script $SERVER $1 $2
 }
 
 clean () {
@@ -17,7 +41,21 @@ clean () {
     run "rm -rf $REMOTE_WORK_DIR"
 }
 
-#make parallelism
+# aws calls
+
+run_aws () {
+    run_impl $AWS_SERVER $@
+}
+
+run_script_aws () {
+    run_script $AWS_SERVER $1 $2
+}
+
+#############
+# SHARED VARS
+#############
+
+# make parallelism
 NPROC=8
 
 # remote variables
@@ -28,6 +66,12 @@ REMOTE_CHIPYARD_DIR=$REMOTE_WORK_DIR/chipyard
 REMOTE_VERILATOR_DIR=$REMOTE_WORK_DIR/verilator
 REMOTE_SIM_DIR=$REMOTE_CHIPYARD_DIR/sims/verilator
 REMOTE_JAVA_ARGS="-Xmx8G -Xss8M -Dsbt.ivy.home=$REMOTE_WORK_DIR/.ivy2 -Dsbt.global.base=$REMOTE_WORK_DIR/.sbt -Dsbt.boot.directory=$REMOTE_WORK_DIR/.sbt/boot"
+REMOTE_AWS_WORK_DIR=$CI_DIR/$CIRCLE_PROJECT_REPONAME-$CIRCLE_BRANCH-$CIRCLE_SHA1-$CIRCLE_JOB
+REMOTE_AWS_CHIPYARD_DIR=$REMOTE_AWS_WORK_DIR/chipyard
+REMOTE_AWS_FSIM_DIR=$REMOTE_AWS_CHIPYARD_DIR/sims/firesim
+REMOTE_AWS_MARSHAL_DIR=$REMOTE_CHIPYARD_DIR/software/firemarshal
+REMOTE_AWS_RESULTS_DIR=$REMOTE_AWS_FSIM_DEPLOY_DIR/results_workload
+REMOTE_AWS_FSIM_DEPLOY_DIR=$REMOTE_AWS_FSIM_DIR/deploy
 
 # local variables (aka within the docker container)
 LOCAL_CHECKOUT_DIR=$HOME/project
@@ -37,12 +81,5 @@ LOCAL_CHIPYARD_DIR=$HOME/chipyard
 LOCAL_VERILATOR_DIR=$HOME/verilator
 LOCAL_SIM_DIR=$LOCAL_CHIPYARD_DIR/sims/verilator
 
-# key value store to get the build strings
-declare -A mapping
-mapping["smallboom"]="CONFIG=SmallBoomConfig"
-mapping["mediumboom"]="CONFIG=MediumBoomConfig"
-mapping["largeboom"]="CONFIG=LargeBoomConfig"
-mapping["megaboom"]="CONFIG=MegaBoomConfig"
-mapping["boomandrocket"]="CONFIG=SmallBoomAndRocketConfig"
-mapping["rv32boom"]="CONFIG=SmallRV32BoomConfig"
-mapping["hwachaboom"]="CONFIG=HwachaLargeBoomConfig"
+# obtain the key value store for the builds
+./build-store.sh

riscv-boom/riscv-boom

.circleci/defaults.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
+# shared variables between the different services
+#
+# CircleCI set values:
+#   $SERVER - points to the millennium build server
+#   $AWS_SERVER - points to the aws manager instance
+#   $CI_DIR - home directory on build server
+#   $CI_AWS_DIR - home directory on aws
+
+##################
+# SHARED FUNCTIONS
+##################
+
+# shared across build server and aws
+
 copy () {
     rsync -avzp -e 'ssh' $1 $2
 }
 
+run_impl () {
+    ssh -o "StrictHostKeyChecking no" -t $1 $2
+}
+
+run_script_impl () {
+    ssh -o "StrictHostKeyChecking no" -t $1 'bash -s' < $2 "$3"
+}
+
+# build server calls
+
 run () {
-    ssh -o "StrictHostKeyChecking no" -t $SERVER $@
+    run_impl $SERVER $@
 }
 
 run_script () {
-    ssh -o "StrictHostKeyChecking no" -t $SERVER 'bash -s' < $1 "$2"
+    run_script_impl $SERVER $1 $2
 }
 
 clean () {
@@ -17,7 +41,21 @@ clean () {
     run "rm -rf $REMOTE_WORK_DIR"
 }
 
-#make parallelism
+# aws calls
+
+run_aws () {
+    run_impl $AWS_SERVER $@
+}
+
+run_script_aws () {
+    run_script_impl $AWS_SERVER $1 $2
+}
+
+#############
+# SHARED VARS
+#############
+
+# make parallelism
 NPROC=8
 
 # remote variables
@@ -28,6 +66,12 @@ REMOTE_CHIPYARD_DIR=$REMOTE_WORK_DIR/chipyard
 REMOTE_VERILATOR_DIR=$REMOTE_WORK_DIR/verilator
 REMOTE_SIM_DIR=$REMOTE_CHIPYARD_DIR/sims/verilator
 REMOTE_JAVA_ARGS="-Xmx8G -Xss8M -Dsbt.ivy.home=$REMOTE_WORK_DIR/.ivy2 -Dsbt.global.base=$REMOTE_WORK_DIR/.sbt -Dsbt.boot.directory=$REMOTE_WORK_DIR/.sbt/boot"
+REMOTE_AWS_WORK_DIR=$CI_AWS_DIR/$CIRCLE_PROJECT_REPONAME-$CIRCLE_BRANCH-$CIRCLE_SHA1-$CIRCLE_JOB
+REMOTE_AWS_CHIPYARD_DIR=$REMOTE_AWS_WORK_DIR/chipyard
+REMOTE_AWS_FSIM_DIR=$REMOTE_AWS_CHIPYARD_DIR/sims/firesim
+REMOTE_AWS_MARSHAL_DIR=$REMOTE_CHIPYARD_DIR/software/firemarshal
+REMOTE_AWS_RESULTS_DIR=$REMOTE_AWS_FSIM_DEPLOY_DIR/results_workload
+REMOTE_AWS_FSIM_DEPLOY_DIR=$REMOTE_AWS_FSIM_DIR/deploy
 
 # local variables (aka within the docker container)
 LOCAL_CHECKOUT_DIR=$HOME/project