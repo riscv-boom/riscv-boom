cda845f390b4178e353cb216b364f93f559ad836
Ben Korpan <bkorpan@berkeley.edu>
2020-03-11 09:46:17 -0700

    Need to send valids to scheduler as well as the 'fire' signals.

--- src/main/scala/exu/core.scala
+++ src/main/scala/exu/core.scala
@@ -716,7 +716,8 @@ class BoomCore(implicit p: Parameters) extends BoomModule
     dispatcher.io.ren_uops(w).bits  := dis_uops(w)
   }
 
-  scheduler.io.dis_uops <> dispatcher.io.dis_uops(issueParams.indexWhere(_.iqType == IQT_INT.litValue))
+  scheduler.io.dis_uops   <> dispatcher.io.dis_uops(issueParams.indexWhere(_.iqType == IQT_INT.litValue))
+  scheduler.io.dis_valids := dis_valids zip dis_uops map { case (v,u) => v && u.iq_type === IQT_INT }
   if (usingFPU) fp_pipeline.io.dis_uops <> dispatcher.io.dis_uops(issueParams.indexWhere(_.iqType == IQT_FP.litValue))
 
   //-------------------------------------------------------------
