c47fab5b655c743ee36602bcd191d19fb06e5341
Jerry Zhao <jerryz123@berkeley.edu>
2020-04-20 19:32:24 -0700

    Update CHIPYARD.hash

--- .circleci/do-rtl-build.sh
+++ .circleci/do-rtl-build.sh
@@ -21,11 +21,9 @@ run "echo \"Ping $SERVER\""
 
 clean
 
-# copy over riscv/esp-tools, verilator, and chipyard to remote
+# copy over riscv/esp-tools, and chipyard to remote
 run "mkdir -p $REMOTE_CHIPYARD_DIR"
-run "mkdir -p $REMOTE_VERILATOR_DIR"
 copy $LOCAL_CHIPYARD_DIR/ $SERVER:$REMOTE_CHIPYARD_DIR
-copy $LOCAL_VERILATOR_DIR/ $SERVER:$REMOTE_VERILATOR_DIR
 
 run "cp -r ~/.ivy2 $REMOTE_WORK_DIR"
 run "cp -r ~/.sbt  $REMOTE_WORK_DIR"
@@ -45,8 +43,7 @@ fi
 # enter the verilator directory and build the specific config on remote server
 run "make -C $REMOTE_SIM_DIR clean"
 run "export RISCV=\"$TOOLS_DIR\"; export LD_LIBRARY_PATH=\"$LD_LIB_DIR\"; \
-     export VERILATOR_ROOT=$REMOTE_VERILATOR_DIR/install/share/verilator; \
-     make -C $REMOTE_SIM_DIR VERILATOR_INSTALL_DIR=$REMOTE_VERILATOR_DIR JAVA_ARGS=\"-Xmx16G -Xss8M\" ${mapping[$1]}"
+     make -C $REMOTE_SIM_DIR JAVA_ARGS=\"-Xmx16G -Xss8M\" ${mapping[$1]}"
 run "rm -rf $REMOTE_CHIPYARD_DIR/project"
 
 # copy back the final build
