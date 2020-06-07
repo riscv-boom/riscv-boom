626fe585d0e503be76b2f059ea58fce07d502329
Jerry Zhao <jerryz123@berkeley.edu>
2020-06-06 22:41:44 -0700

    [dcache] Reduce MSHR response critical path

--- src/main/scala/lsu/dcache.scala
+++ src/main/scala/lsu/dcache.scala
@@ -833,12 +833,12 @@ class BoomNonBlockingDCacheModule(outer: BoomNonBlockingDCache) extends LazyModu
   val uncache_resp = Wire(Valid(new BoomDCacheResp))
   uncache_resp.bits     := mshrs.io.resp.bits
   uncache_resp.valid    := mshrs.io.resp.valid
-  mshrs.io.resp.ready := !(cache_resp.map(_.valid).reduce(_&&_)) // We can backpressure the MSHRs, but not cache hits
+  mshrs.io.resp.ready   := !(s2_valid.reduce(_&&_)) // We can backpressure the MSHRs, but not cache hits
 
   val resp = WireInit(cache_resp)
   var uncache_responding = false.B
   for (w <- 0 until lsuWidth) {
-    val uncache_respond = !cache_resp(w).valid && !uncache_responding
+    val uncache_respond = !s2_valid(w) && !uncache_responding
     when (uncache_respond) {
       resp(w) := uncache_resp
     }
