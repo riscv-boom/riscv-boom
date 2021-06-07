845e3ee0cd8735a079153011fd425de320fdcdba
Jerry Zhao <jerryz123@berkeley.edu>
2021-06-07 10:59:50 -0700

    Cut up a couple more paths

--- src/main/scala/common/tile.scala
+++ src/main/scala/common/tile.scala
@@ -96,7 +96,7 @@ class BoomTile private(
   tile_master_blocker.foreach(lm => connectTLSlave(lm.controlNode, xBytes))
 
   // TODO: this doesn't block other masters, e.g. RoCCs
-  tlOtherMastersNode := tile_master_blocker.map { _.node := tlMasterXbar.node } getOrElse { tlMasterXbar.node }
+  tlOtherMastersNode := TLBuffer() := tile_master_blocker.map { _.node := tlMasterXbar.node } getOrElse { tlMasterXbar.node }
   masterNode :=* tlOtherMastersNode
 
   val cpuDevice: SimpleDevice = new SimpleDevice("cpu", Seq("ucb-bar,boom0", "riscv")) {
