dcd4b347a86e1172017104bc98cf84b13daa030b
Jennifer Zhou <jennifersiyuanzhou@berkeley.edu>
2022-02-28 09:32:01 -0800

    clean up bitmanip.scala a bit

--- src/main/scala/exu/execution-units/bitmanip.scala
+++ src/main/scala/exu/execution-units/bitmanip.scala
@@ -17,7 +17,7 @@ import boom.util._
 
 object BITMANIP {
     val SZ_BITMANIP_FN = 5
-    def FN_X    = BitPat("b????")
+    def FN_X    = BitPat("b?????")
     def FN_ANDN = 0.U(SZ_BITMANIP_FN.W) // had to add .W or else it complains
     def FN_ORN  = 1.U(SZ_BITMANIP_FN.W)
     def FN_XNOR = 2.U(SZ_BITMANIP_FN.W)
@@ -48,7 +48,7 @@ object BITMANIP {
 import BITMANIP._
 
 class BITMANIP(implicit p: Parameters) extends CoreModule()(p) {
-  val io = IO(new Bundle { //wrap IO 
+  val io = IO(new Bundle {
     val dw = Input(UInt(SZ_DW.W))
     val fn = Input(UInt(SZ_BITMANIP_FN.W))
     val in2 = Input(UInt(xLen.W))
@@ -59,31 +59,34 @@ class BITMANIP(implicit p: Parameters) extends CoreModule()(p) {
     val orn = io.in1 | ~io.in2
     val xnor = ~(io.in1 ^ io.in2)
 
+    val count_operand = Mux(io.fn === FN_CLZ, Reverse(io.in1), io.in1)
+    val count_operand_msb = PriorityEncoder(count_operand(63,32))
+    val count_operand_lsb = PriorityEncoder(count_operand(31,0))
     //clz, clzw
-    val leading_reverse = Reverse(io.in1)
-    val clz_msb =  PriorityEncoder(leading_reverse(63,32))
-    val clz_lsb =  PriorityEncoder(leading_reverse(31,0))
-
+    val clz_msb =  count_operand_msb
+    val clz_lsb =  count_operand_lsb
     val clzw =  Mux(clz_msb === 31.U && io.in1(0) === 0.U, 32.U, clz_msb)
     val clz_lsb_res = Mux(clz_lsb === 31.U && io.in1(32) === 0.U, 32.U, clz_lsb)
     val clz = Mux(clz_lsb_res === 32.U, clzw +& clz_lsb_res, clz_lsb_res)
 
     //ctz, ctzw
-    val ctz_msb =  PriorityEncoder(io.in1(63,32))
-    val ctz_lsb =  PriorityEncoder(io.in1(31,0))
+    val ctz_msb =  count_operand_msb
+    val ctz_lsb =  count_operand_lsb
     val ctzw =  Mux(ctz_lsb === 31.U && io.in1(31) === 0.U, 32.U, ctz_lsb)
     val ctz_msb_res = Mux(ctz_msb === 31.U && io.in1(63) === 0.U, 32.U, ctz_msb)
     val ctz = Mux(ctzw === 32.U, ctzw +& ctz_msb_res, ctzw)
 
-    val pcnt = PopCount(io.in1) //do math
+    // pcnt, pcntw
+    val pcnt = PopCount(io.in1)
     val pcntw = PopCount(io.in1(31,0))
 
-    //try single comparator sign and unsigned
+    // max, maxu, min, minu
     val max = Mux(io.in1.asSInt < io.in2.asSInt, io.in2, io.in1)
     val maxu = Mux(io.in1 < io.in2, io.in2, io.in1)
     val min = Mux(io.in1.asSInt < io.in2.asSInt, io.in1, io.in2)
     val minu = Mux(io.in1 < io.in2, io.in1, io.in2)
 
+    // sextb, sexth, zexth
     val sextb = Cat(Fill(56, io.in1(7)),io.in1(7,0))
     val sexth = Cat(Fill(48, io.in1(15)),io.in1(15,0))
     val zexth = Cat(Fill(48, "b0".U),io.in1(15,0))
@@ -112,16 +115,18 @@ class BITMANIP(implicit p: Parameters) extends CoreModule()(p) {
     val shamt_rorw = io.in2(4,0)
     val rorw = ((in1_mask >> shamt_rorw) | (in1_mask << (32.U - shamt_rorw))) & mask
 
-    val orcb0 = Mux(io.in1(7,0) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb1 = Mux(io.in1(15,8) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb2 = Mux(io.in1(23,16) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb3 = Mux(io.in1(31,24) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb4 = Mux(io.in1(39,32) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb5 = Mux(io.in1(47,40) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb6 = Mux(io.in1(55,48) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
-    val orcb7 = Mux(io.in1(63,56) === "b_0000_0000".U, "b_0000_0000".U, "b_1111_1111".U)
+    //orcb
+    val orcb0 = Fill(8, io.in1(7,0).orR)
+    val orcb1 = Fill(8, io.in1(15,8).orR)
+    val orcb2 = Fill(8, io.in1(23,16).orR)
+    val orcb3 = Fill(8, io.in1(31,24).orR)
+    val orcb4 = Fill(8, io.in1(39,32).orR)
+    val orcb5 = Fill(8, io.in1(47,40).orR)
+    val orcb6 = Fill(8, io.in1(55,48).orR)
+    val orcb7 = Fill(8, io.in1(63,56).orR)
     val orb = Cat(orcb7, orcb6, orcb5, orcb4, orcb3, orcb2, orcb1, orcb0)
-
+    
+    //rev
     val rev0 = io.in1(7,0)
     val rev1 = io.in1(15,8)
     val rev2 = io.in1(23,16)
@@ -132,31 +137,32 @@ class BITMANIP(implicit p: Parameters) extends CoreModule()(p) {
     val rev7 = io.in1(63,56)
     val rev8 = Cat(rev0, rev1, rev2, rev3, rev4, rev5, rev6, rev7)
 
+    //pack
     val pack = Cat(io.in2(31,0), io.in1(31,0))
 
-    io.out := MuxCase("h_dead_beef_dead_beef".U, 
-                Array((io.fn === 0.U)                   -> andn,
-                      (io.fn === 1.U)                   -> orn, 
-                      (io.fn === 2.U)                   -> xnor,
-                      (io.fn === 3.U && io.dw === 1.U)  -> clz,
-                      (io.fn === 3.U && io.dw === 0.U)  -> clzw,
-                      (io.fn === 4.U && io.dw === 1.U)  -> ctz,
-                      (io.fn === 4.U && io.dw === 0.U)  -> ctzw,
-                      (io.fn === 5.U && io.dw === 1.U)  -> pcnt,
-                      (io.fn === 5.U && io.dw === 0.U)  -> pcntw,
-                      (io.fn === 6.U && io.dw === 1.U)  -> max,
-                      (io.fn === 7.U && io.dw === 1.U)  -> maxu,
-                      (io.fn === 8.U && io.dw === 1.U)  -> min,
-                      (io.fn === 9.U && io.dw === 1.U)  -> minu,
-                      (io.fn === 10.U && io.dw === 1.U)  -> sextb,
-                      (io.fn === 11.U && io.dw === 1.U)  -> sexth,
-                      (io.fn === 12.U && io.dw === 1.U)  -> zexth,
-                      (io.fn === 13.U && io.dw === 1.U)  -> rol,
-                      (io.fn === 13.U && io.dw === 0.U)  -> rolw,
-                      (io.fn === 14.U && io.dw === 1.U)  -> ror, //ror and rori same
-                      (io.fn === 14.U && io.dw === 0.U)  -> rorw, //rorw and roriw same
-                      (io.fn === 15.U && io.dw === 1.U)  -> orb,
-                      (io.fn === 16.U && io.dw === 1.U)  -> rev8,
-                      (io.fn === 17.U && io.dw === 1.U)  -> pack)
+    io.out := MuxCase("h_dead_beef_dead_beef".U,
+                Array((io.fn === FN_ANDN)                              -> andn,
+                      (io.fn === FN_ORN)                               -> orn,
+                      (io.fn === FN_XNOR)                              -> xnor,
+                      (io.fn === FN_CLZ   && io.dw === DW_64)  -> clz,
+                      (io.fn === FN_CLZ   && io.dw === DW_32)  -> clzw,
+                      (io.fn === FN_CTZ   && io.dw === DW_64)  -> ctz,
+                      (io.fn === FN_CTZ   && io.dw === DW_32)  -> ctzw,
+                      (io.fn === FN_PCNT  && io.dw === DW_64)  -> pcnt,
+                      (io.fn === FN_PCNT  && io.dw === DW_32)  -> pcntw,
+                      (io.fn === FN_MAX   && io.dw === DW_64)  -> max,
+                      (io.fn === FN_MAXU  && io.dw === DW_64)  -> maxu,
+                      (io.fn === FN_MIN   && io.dw === DW_64)  -> min,
+                      (io.fn === FN_MINU  && io.dw === DW_64)  -> minu,
+                      (io.fn === FN_SEXTB && io.dw === DW_64)  -> sextb,
+                      (io.fn === FN_SEXTH && io.dw === DW_64)  -> sexth,
+                      (io.fn === FN_ZEXTH && io.dw === DW_64)  -> zexth,
+                      (io.fn === FN_ROL   && io.dw === DW_64)  -> rol,
+                      (io.fn === FN_ROL   && io.dw === DW_32)  -> rolw,
+                      (io.fn === FN_ROR   && io.dw === DW_64)  -> ror, //ror and rori same
+                      (io.fn === FN_ROR   && io.dw === DW_32)  -> rorw, //rorw and roriw same
+                      (io.fn === FN_ORCB  && io.dw === DW_64)  -> orb,
+                      (io.fn === FN_REV8  && io.dw === DW_64)  -> rev8,
+                      (io.fn === FN_PACK  && io.dw === DW_64)  -> pack)
                 )
 }
