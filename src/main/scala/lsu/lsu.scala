426bcf9c2aed15063d1ccbb53cfb7a5a0c9fc0bb
plutsyk <115722098+plutsyk@users.noreply.github.com>
2023-01-04 09:33:12 +0100

    Update lsu.scala (#620)
    fix the order of operands to MUX that updates the ``store_blocked_counter``

--- src/main/scala/lsu/lsu.scala
+++ src/main/scala/lsu/lsu.scala
@@ -1207,7 +1207,7 @@ class LSU(implicit p: Parameters, edge: TLEdgeOut) extends BoomModule()(p)
     when (will_fire_store_commit(0) || !can_fire_store_commit(0)) {
       store_blocked_counter := 0.U
     } .elsewhen (can_fire_store_commit(0) && !will_fire_store_commit(0)) {
-      store_blocked_counter := Mux(store_blocked_counter === 15.U, store_blocked_counter + 1.U, 15.U)
+      store_blocked_counter := Mux(store_blocked_counter === 15.U, 15.U, store_blocked_counter + 1.U)
     }
     when (store_blocked_counter === 15.U) {
       block_load_wakeup := true.B
