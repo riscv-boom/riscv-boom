f7da4268b8f8efac1ac929dd0dcd831f9cb4c7d0
Jerry Zhao <jerryz123@berkeley.edu>
2024-01-29 01:08:51 -0800

    Bump to rocket-chip ifv

--- src/main/scala/common/parameters.scala
+++ src/main/scala/common/parameters.scala
@@ -119,7 +119,6 @@ case class BoomCoreParams(
   val useCryptoSM = false
   val traceHasWdata = trace
   val useConditionalZero = false
-  val useVector = false
   override def customCSRs(implicit p: Parameters) = new BoomCustomCSRs
 }
 
